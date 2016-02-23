91315197f88913a99ba770dd81cbd3fe1cf467c9
wilfeli <wilfeli@gmail.com>
2016-02-22 23:55:24 -0800

    initial architecture

---
+++ Source/ABMSolar/MarketingSystem.h
@@ -0,0 +1,13 @@
+//
+//  MarketingSystem.h
+//  ABMSolar
+//
+//  Created by Ekaterina Sinitskaya on 2/22/16.
+//  Copyright (c) 2016 wilfeli. All rights reserved.
+//
+
+#ifndef ABMSolar_MarketingSystem_h
+#define ABMSolar_MarketingSystem_h
+
+
+#endif
