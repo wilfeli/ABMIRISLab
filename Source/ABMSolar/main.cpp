a14789861964fea0cccb7f04a1a6076e1dad6950
wilfeli <wilfeli@gmail.com>
2016-02-22 23:28:39 -0800

    initial architecture

---
+++ Source/ABMSolar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ABMSolar
+//
+//  Created by Ekaterina Sinitskaya on 2/22/16.
+//  Copyright (c) 2016 wilfeli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
