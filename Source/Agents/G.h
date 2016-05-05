726ead22a454da4aa182e5a1d26171b2e02a460b
kjgomez <kjgomez@stanford.edu>
2016-05-05 16:00:46 -0700

    Working on reviewing G files
    Former-commit-id: 4954eb9d2dcd2b5e387b7d6d15bd9ac2d7829fe0

--- Source/Agents/G.h
+++ Source/Agents/G.h
@@ -1,4 +1,4 @@
-//
+//  Kelley reviewed 4/26/16 line 17, 22, 24, 49, 65, 67-69, 73-75, 97
 //  G.h
 //  ABMSolar
 //
@@ -14,15 +14,16 @@
 
 #include "Tools/IParameters.h"
 
+//initially when I was reviewing this file, I didn't understand what G was; this may become clear once we add parameters but maybe changing name of class?
 
 namespace solar_core
 {
-    
+   
 class PVProject;
+//why do we call PVProject here? Why not call on agent SolarPanel?
     
-    
-    
-class G
+
+class G 
 {
 public:
     //@{
@@ -43,11 +44,9 @@ public:
      
      */
     virtual void request_permit(std::shared_ptr<PVProject> project_); /*!< request permit for the project */
-    
     //@}
     
-    
-    
+    //add virtual void request_inspection
     
     
     
@@ -63,14 +62,17 @@ protected:
      
      */
     
-    std::deque<std::shared_ptr<PVProject>> pending_projects;
+    std::deque<std::shared_ptr<PVProject>> pending_projects; //projects are in pending state twice, during permitting and during inspection
     
-    std::vector<std::vector<std::weak_ptr<PVProject>>> schedule_visits; /*!< schedule for visits for the permitting accessment, length is equal to the MaxLengthWaitPermitVisit */
+    std::vector<std::vector<std::weak_ptr<PVProject>>> schedule_visits; /*!< schedule for visits for the permitting assessment, length is equal to the MaxLengthWaitPermitVisit */
+    //because of lack of standardization, some jurisdictions require visits while others do not
+	//should jurisdiction be considered a class, kind of like PVProject to account for wait times, online/in-person turn-in requirements, etc. or can this still be modeled as an agent?
+    std::size_t i_schedule_visits; //this would most likely be for inspection
     
-    std::size_t i_schedule_visits;
-    
-    void collect_inf_site_visit(std::shared_ptr<PVProject> project_);
-    void grant_permit(std::shared_ptr<PVProject> project_);
+
+    void collect_inf_site_visit(std::shared_ptr<PVProject> project_); //what does this void mean?
+    void grant_permit(std::shared_ptr<PVProject> project_); //I think better to rename approve_permit
+	void pass_inspection(std::shared_ptr<PVProject> project_); //added this because this is one more area of interaction between SEI and G
     
     
     //@}
@@ -90,9 +92,9 @@ protected:
     void ac_update_tick();
     
     
-    W* w;
+    W* w; 
     
-    TimeUnit a_time;
+    TimeUnit a_time; //Is this initialized? Because it is a typedef, is there a special way to initialize? it is a piece of memory, right?
     //@}
     
     
