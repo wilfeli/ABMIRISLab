f09392d42145b04513cbdfea054ba1b0e09ac197
wilfeli <wilfeli@gmail.com>
2016-05-10 21:17:01 -0700

    working on permitting

---
+++ Source/Agents/Utility.cpp
@@ -0,0 +1,9 @@
+//
+//  Utility.cpp
+//  ABMSolar
+//
+//  Created by Ekaterina Sinitskaya on 5/10/16.
+//  Copyright (c) 2016 IRIS Lab. All rights reserved.
+//
+
+#include "Agents/Utility.h"
---
+++ Source/Agents/Utility.h
@@ -0,0 +1,14 @@
+//
+//  Utility.h
+//  ABMSolar
+//
+//  Created by Ekaterina Sinitskaya on 5/10/16.
+//  Copyright (c) 2016 IRIS Lab. All rights reserved.
+//
+
+#ifndef __ABMSolar__Utility__
+#define __ABMSolar__Utility__
+
+
+
+#endif /* defined(__ABMSolar__Utility__) */
